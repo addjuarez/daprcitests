d62ef363d0728cbf57b0c414bab5202120108ea8
addjuarez <adjuarez@microsoft.com>
2022-10-30 10:48:58 -0500

    Update dapr_tests.mk

--- tests/dapr_tests.mk
+++ tests/dapr_tests.mk
@@ -381,7 +381,7 @@ endif
 
 # install redis to the cluster without password
 setup-test-env-redis:
-	$(HELM) install dapr-redis bitnami/redis --wait --timeout 5m0s --namespace $(DAPR_TEST_NAMESPACE) -f ./tests/config/redis_override.yaml
+	$(HELM) install dapr-redis bitnami/redis --debug --wait --timeout 5m0s --namespace $(DAPR_TEST_NAMESPACE) -f ./tests/config/redis_override.yaml
 delete-test-env-redis:
 	${HELM} del dapr-redis --namespace ${DAPR_TEST_NAMESPACE}
 
